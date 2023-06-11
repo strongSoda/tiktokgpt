20c0ab6f6b2d49043a79ae4ba47d21f5f6256757
strongSoda <strongsoda2@gmail.com>
2023-06-11 12:04:44 +0530

    fix

--- src/pages/api/render.js
+++ src/pages/api/render.js
@@ -24,7 +24,7 @@ const start = async () => {
 
   // You only have to do this once, you can reuse the bundle.
   console.log("Creating a Webpack bundle of the video");
-  const entry = "src/remotion/index.js";
+  const entry = "@/remotion/index.js";
   const bundleLocation = await bundle(path.resolve(entry), () => undefined, {
     // If you have a Webpack override, make sure to add it here
     webpackOverride: (config) => config,
