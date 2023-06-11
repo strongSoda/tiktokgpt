b3feabb60ce8f74278a76f6b1b14318b039ce094
strongSoda <strongsoda2@gmail.com>
2023-06-11 15:10:36 +0530

    add fonts to Chakra theme

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,18 @@
 // pages/_app.js
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+import '@fontsource/raleway/400.css'
+import '@fontsource/open-sans/700.css'
+
+const theme = extendTheme({
+  fonts: {
+    heading: `'Raleway', sans-serif`,
+    body: `'Open Sans', sans-serif`,
+  },
+})
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   )
