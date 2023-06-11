9770d6927916f97a203c6a8312afc49eb4ad48fa
strongSoda <strongsoda2@gmail.com>
2023-06-11 16:11:32 +0530

    add action buttons, make responsive, improve UI

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,49 +3,86 @@ import { Player } from "@remotion/player";
 import { MyComposition } from "@/remotion/Composition";
 import subtitles from "@/data/subtitles";
 import Nav from "@/app/components/Nav";
-import { Heading, Box, Button, Image } from '@chakra-ui/react'
+import { Heading, Box, Button, Image, Stack, Text } from '@chakra-ui/react'
 import { RenderLoading, RenderPoster } from "@/app/components/RenderAssets";
+import { TriangleUpIcon, AddIcon, TriangleDownIcon } from '@chakra-ui/icons';
+import Footer from "@/app/components/Footer";
 
 const Home = () => {
 
 return (
     <>
         <Nav />
-        <Box className="container" w='60%' ml='auto' mr='auto' mt='0' mb='0' p='2'>
+        <Box className="container" w={['200vw', '100vw', '100%', '100%']}
+         ml='auto' mr='auto' mt='0' mb='10' p='2'>
             <Heading as='h1' size='3xl' className="title" textAlign='center' mt='10' mb='10'>
                 My Feed
             </Heading>
 
-            <Box w='lg' className="video-container" ml='auto' mr='auto'>
-                <Player
-                    component={MyComposition}
-                    inputProps={{ subtitles: subtitles }}
-                    durationInFrames={1200}
-                    compositionWidth={515}
-                    compositionHeight={720}
-                    fps={30}
-                    controls
-                    renderLoading={RenderLoading}
-                    renderPoster={RenderPoster}
-                    showPosterWhenUnplayed
-                    showPosterWhenPaused
-                    showPosterWhenEnded
-                    />
-            
-                <Button 
-                    w='lg'
-                    mt='10'
-                    ml='auto'
-                    mr='auto'
-                    colorScheme="teal"
+            <Stack direction="row" spacing={2} align="center" width="lg" ml='auto' mr='auto'>
+                <Box className="video-container">
+                    <Player
+                        component={MyComposition}
+                        inputProps={{ subtitles: subtitles }}
+                        durationInFrames={1200}
+                        compositionWidth={515}
+                        compositionHeight={720}
+                        fps={30}
+                        controls
+                        renderLoading={RenderLoading}
+                        renderPoster={RenderPoster}
+                        showPosterWhenUnplayed
+                        showPosterWhenPaused
+                        showPosterWhenEnded
+                        />
+                
+                    <Button 
+                        w='lg'
+                        mt='10'
+                        ml='auto'
+                        mr='auto'
+                        colorScheme="teal"
+                        
+                        onClick={() => {
+                            window.open('/api/render', '_blank');
+                        }}
+                    >Render Video</Button>
+                </Box>
+
+                <Stack direction="column" spacing={8} align="center">
                     
-                    onClick={() => {
-                        window.open('/api/render', '_blank');
-                    }}
-                >Render Video</Button>
-            </Box>
+                    <Stack direction="column" spacing={1} align="center">
+                        <Button>
+                            <TriangleUpIcon />
+                        </Button>
+                        <Text>
+                            4.5k
+                        </Text>
+                    </Stack>
+
+                    <Stack direction="column" spacing={1} align="center">
+                        <Button>
+                            <TriangleDownIcon />
+                        </Button>
+                        <Text>
+                            20M
+                        </Text>
+                    </Stack>
+
+                    <Stack direction="column" spacing={1} align="center">
+                        <Button>
+                            <AddIcon />
+                        </Button>
+                        <Text>
+                            Share
+                        </Text>
+                    </Stack>
+                </Stack>
+            </Stack>
 
         </Box>
+
+        <Footer />
     </>
 )
 }
