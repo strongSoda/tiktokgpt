c6a6c9e93ee9c7083261bb9eba91970f2479bf88
strongSoda <strongsoda2@gmail.com>
2023-06-11 15:10:26 +0530

    restyle Composition

--- src/remotion/Composition.js
+++ src/remotion/Composition.js
@@ -1,28 +1,29 @@
 import { interpolate, Sequence, spring, staticFile, useCurrentFrame, useVideoConfig, Video } from "remotion";
-import { Heading } from '@chakra-ui/react'
-import { loadFont } from "@remotion/google-fonts/TitanOne";
-import { useEffect } from "react";
+import { Heading, Box } from '@chakra-ui/react'
 import subtitles from "../data/subtitles";
+
+import { loadFont } from "@remotion/google-fonts/TitanOne";
 const { fontFamily } = loadFont();
 
 export const MyComposition = ({}) => {
     const {fps, durationInFrames} = useVideoConfig();
 
     return (
-    <>
-     <Sequence from={0} durationInFrames={durationInFrames}>
-        <Video 
-        style={{width: '100%', height: '100%'}}
-        width="100%" src={"https://repo.library.stonybrook.edu/xmlui/bitstream/handle/11401/9656/rickroll.mp4?sequence=1&isAllowed=y"} />;
-    </Sequence> 
-    {subtitles?.map((subtitle) => (
-        <Sequence key={subtitle?.id} from={subtitle?.start} durationInFrames={subtitle?.duration}>
-             <Subtitle text={subtitle?.text} />
-        </Sequence>
-    ))}
-    </>
-    )
-    };
+        <>
+        <Sequence from={0} durationInFrames={durationInFrames}>
+            <Box className="video" w='lg' ml='auto' mr='auto' mt='0' mb='0' bg='black' borderWidth='1px' borderRadius='lg'>
+                <Video 
+                style={{width: '100%', height: '100%'}}
+                src={"https://repo.library.stonybrook.edu/xmlui/bitstream/handle/11401/9656/rickroll.mp4?sequence=1&isAllowed=y"} />
+            </Box>
+        </Sequence> 
+        {subtitles?.map((subtitle) => (
+            <Sequence key={subtitle?.id} from={subtitle?.start} durationInFrames={subtitle?.duration}>
+                <Subtitle text={subtitle?.text} />
+            </Sequence>
+        ))}
+        </>
+)};
 
 
 export const Subtitle = ({text}) => {
@@ -31,21 +32,21 @@ export const Subtitle = ({text}) => {
 
     const opacity = interpolate(frame, [0, 30], [0, 1])
     const translate = spring({ frame, fps, from: 0, to: 100 })
+
     return (
         <Heading as='h1'
-            maxW='50%'
-            ml='25%'
+            maxW='100%'
             style={{
                 position: 'absolute',
-                width: 'fit-content',
+                width: '100%',
                 textAlign: 'center',
                 opacity,
-                top: '30%',
+                top: '50%',
                 color: 'white',
                 fontFamily: fontFamily,
-                fontSize: '3rem',
+                fontSize: '2rem',
                 transform: `translateY(-${translate}px)`
-            }} size='4xl' className="subtitle">
+            }} size='xl' className="subtitle">
                 {text}
         </Heading>
     )
